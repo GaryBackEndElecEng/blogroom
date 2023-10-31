837284aa46ea1ecdf77bb2c5c2cdc3662943d81b
gary wallace <garyatgreatvacation4u@gmail.com>
2023-10-31 15:46:04 -0400

    upgraded npm

--- src/components/posts/Msg.tsx
+++ src/components/posts/Msg.tsx
@@ -6,7 +6,7 @@ type MsgType = {
     setMsg: React.Dispatch<React.SetStateAction<msgType>>
 }
 export default function Msg({ msg, setMsg }: MsgType) {
-    const msgStyle = msg.loaded ? "text-slate-200 text-3xl " : "text-red-600 text-3xl"
+    const msgStyle = msg.loaded ? "text-slate-200 text-3xl " : "text-red-700 text-3xl"
 
     React.useEffect(() => {
         if (msg && msg.loaded) {
